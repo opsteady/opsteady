f070e02ad4980ac79e1f7e499f43ff88b87e9c0b
Ivo Verberk <iverberk@protonmail.com>
2021-11-13 21:42:48 +0100

    Improve Azure eventual consistency handling

--- capabilities/dns/aws/terraform/terraform.tf
+++ capabilities/dns/aws/terraform/terraform.tf
@@ -29,16 +29,6 @@ provider "aws" {
   region = var.foundation_aws_region
 }
 
-provider "azurerm" {
-  alias = "management"
-
-  client_id       = var.management_client_id
-  client_secret   = var.management_client_secret
-  subscription_id = var.management_subscription_id
-  tenant_id       = var.tenant_id
-  features {}
-}
-
 provider "vault" {
   address = var.vault_address
   token   = var.vault_token
