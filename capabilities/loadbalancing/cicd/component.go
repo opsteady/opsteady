9738fa270ea8154ee4071a54cec88ceea4cf00e7
Armin <armin@coralic.nl>
2022-12-28 11:52:44 +0100

    Working main

--- capabilities/loadbalancing/cicd/component.go
+++ capabilities/loadbalancing/cicd/component.go
@@ -8,25 +8,24 @@ import (
 	kubernetesAzureCluster "github.com/opsteady/opsteady/kubernetes/azure/cluster/cicd"
 )
 
-// CapabilitiesLoadbalacing is an implementation for the Nginx controller
-type CapabilitiesLoadbalacing struct {
+// CapabilitiesLoadBalancing is an implementation for the Nginx controller
+type CapabilitiesLoadBalancing struct {
 	component.DefaultComponent
-	Metadata *component.Metadata
 }
 
-var Instance = &CapabilitiesLoadbalacing{}
+var Instance = &CapabilitiesLoadBalancing{}
 
 func init() {
 	m := component.DefaultMetadata()
-	m.Name = "loadbalacing"
+	m.Name = "loadBalancing"
 	m.Group = component.CapabilitiesBasic
 	m.AddTarget(component.TargetLocal, component.TargetAws, component.TargetAzure)
 	m.AddGroupDependency(component.KubernetesAddons) // Local misses Addons but we can for now calculate the dependency
 	Instance.Metadata = &m
 }
 
-// Configure configures CapabilitiesLoadbalacing before running
-func (c *CapabilitiesLoadbalacing) Configure(defaultComponent component.DefaultComponent) {
+// Configure configures CapabilitiesLoadBalancing before running
+func (c *CapabilitiesLoadBalancing) Configure(defaultComponent component.DefaultComponent) {
 	c.DefaultComponent = defaultComponent
 	c.AddRequiresInformationFrom(foundationAWS.Instance.GetMetadata(), kubernetesAzureCluster.Instance.GetMetadata(), kubernetesAWSCluster.Instance.GetMetadata())
 	c.UseHelm(component.NewHelmChart(
